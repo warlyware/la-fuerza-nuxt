c31718bc6f5f3a139fea8e20f90d7ab30ed090bf
Sanity.io <hello@sanity.io>
2020-07-22 14:08:19 -0700

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+# la-fuerza-nuxt-studio
---
+++ studio/dashboardConfig.js
@@ -0,0 +1,53 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-nuxt-events'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '',
+                  title: 'Sanity Studio',
+                  name: '',
+                  apiId: ''
+                },
+                {
+                  buildHookId: '',
+                  title: 'Events Website',
+                  name: '',
+                  apiId: ''
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/warlyware/la-fuerza-nuxt',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: '', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recently created sessions', order: '_createdAt desc', types: ['session']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
