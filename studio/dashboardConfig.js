003e6628b64736d6a8d0cf36f9b93ced4ea22e92
Dan Ward <danward@gmail.com>
2020-08-12 11:05:25 -0700

    Update studio dash config

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -12,13 +12,13 @@ export default {
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
-                  buildHookId: 'deploy-status',
+                  buildHookId: '',
                   title: 'Content Editor',
                   name: 'la-fuerza-nuxt-studio-78kv3cho',
                   apiId: '885c515a-aad1-4615-a6fb-bd509db74774'
                 },
                 {
-                  buildHookId: 'deploy-status',
+                  buildHookId: '',
                   title: 'La Fuerza Website',
                   name: 'la-fuerza-nuxt-web',
                   apiId: '503abbce-b99e-4322-85e2-ee4012605774'
