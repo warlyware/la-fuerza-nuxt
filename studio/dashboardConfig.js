9db83b2b3a09f60daa4e48420a15d2c19b81925c
Dan Ward <danward@gmail.com>
2020-08-11 18:44:30 -0700

    Update dashboard widget

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -12,16 +12,16 @@ export default {
                 'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
               sites: [
                 {
-                  buildHookId: '',
-                  title: 'Sanity Studio',
-                  name: '',
-                  apiId: ''
+                  buildHookId: 'deploy-status',
+                  title: 'Content Editor',
+                  name: 'la-fuerza-nuxt-studio-78kv3cho',
+                  apiId: '885c515a-aad1-4615-a6fb-bd509db74774'
                 },
                 {
-                  buildHookId: '',
-                  title: 'Events Website',
-                  name: '',
-                  apiId: ''
+                  buildHookId: 'deploy-status',
+                  title: 'La Fuerza Website',
+                  name: 'la-fuerza-nuxt-web',
+                  apiId: '503abbce-b99e-4322-85e2-ee4012605774'
                 }
               ]
             }
