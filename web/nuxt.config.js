ad57d0736324cf9c263898bc51e79b72b1b2881c
Dan Ward <danward@gmail.com>
2020-08-20 18:13:31 -0700

    Kill localhost fallback

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 
   env: {
     MAPQUEST_KEY: process.env.MAPQUEST_KEY,
-    BASE_URL: process.env.BASE_URL || 'http://localhost:3000'
+    BASE_URL: process.env.BASE_URL
   },
 
   /*
