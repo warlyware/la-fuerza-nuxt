ef06751be755ad27bffb9afd9a2f1e5bb6c7f4ee
Dan Ward <danward@gmail.com>
2020-09-08 15:39:08 -0700

    Fix description in meta

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -12,7 +12,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'description', name: 'description', content: 'La Fuerza de Familias Latinas' },
       {
         'property': 'og:title',
         'content': `La Fuerza de Familias Latinas`,
