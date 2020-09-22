7d679352a2532175b4dbe431a629c516f0bc8899
Dan Ward <danward@gmail.com>
2020-09-22 15:39:54 -0700

    Add sanity-webhooks skeleton function

---
+++ web/functions/sanity-webhook/sanity-webhook.js
@@ -0,0 +1,17 @@
+const headers = process.env.IS_LOCAL_DEV ? {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+    'Origin, X-Requested-With, Content-Type, Accept',
+} : {}
+
+
+exports.handler = async event => {
+  const { payload } = event.body
+  console.log({ event, payload })
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({ payload })
+  }
+}
