cb19f02ff640b9d462487fcdb8c6020e0fe26111
Dan Ward <danward@gmail.com>
2020-09-25 15:10:29 -0700

    Update book details layout

--- web/pages/resources/books/_slug.vue
+++ web/pages/resources/books/_slug.vue
@@ -2,9 +2,21 @@
   <div class="pt-8 mx-auto md:pt-32 mb-8 content w-full">
     <LightBox ref="lightbox" :show-thumbs="false"
     :media="lightboxImages" :show-light-box="false" />
-    <h1 class="font-bold text-4xl text-blue italic uppercase text-center max-w-2xl m-auto leading-10 mb-4">
-      {{title[locale]}}
-    </h1>
+
+
+    <div class="w-full flex flex-wrap">
+      <div class="w-full lg:w-1/2 my-1/2 order-last md:order-none">
+        <youtube player-width="100%" player-height="100%"
+        class="video-responsive flex-grow"
+        :video-id="getYoutubeId(videoUrl)"  />
+      </div>
+      <div class="w-full lg:w-1/2 bg-pink text-white flex justify-center items-center leading-tight p-8 font-bold text-4xl">
+        {{title[locale]}}
+      </div>
+    </div>
+
+    <ShareMenu class="w-full flex justify-center mt-4" :share-menu="shareMenu" />
+
     <div class="flex flex-wrap max-w-5xl m-auto px-8 lg:px-0 justify-center mb-4">
       <p class="py-2 w-full text-blue">
         <BlockContent v-if="this[`${locale}Description`]" :blocks="this[`${locale}Description`]" />
@@ -20,14 +32,9 @@
           &nbsp;/ {{publisher}}
         </span>
       </p>
-      <ShareMenu class="w-full flex justify-center" :share-menu="shareMenu" />
     </div>
-    <div class="w-full max-w-5xl m-auto mb-8">
-      <youtube player-width="100%" player-height="100%"
-      class="video-responsive flex-grow"
-      :video-id="getYoutubeId(videoUrl)"  />
-    </div>
-    <div class="w-full max-w-3xl m-auto flex flex-wrap justify-center px-8 md:px-4 lg:px-0 text-blue mb-8">
+    <Divider class="max-w-6xl m-auto -my-8 px-4 md:px-0" />
+    <div class="w-full max-w-3xl m-auto flex flex-wrap justify-center px-8 md:px-4 lg:px-0 text-blue my-8">
       <BlockContent v-if="this[`${locale}Excerpt`]" :blocks="this[`${locale}Excerpt`]" />
     </div>
     <div v-if="image1"
@@ -48,6 +55,7 @@ import sanityClient from '~/sanityClient'
 const builder = imageUrlBuilder(sanityClient)
 import LightBox from 'vue-image-lightbox'
 import ShareMenu from '~/components/ShareMenu'
+import Divider from '~/components/blocks/Divider'
 import SanityImage from '~/components/SanityImage'
 import getYoutubeId from 'get-youtube-id'
 import BlockContent from 'sanity-blocks-vue-component'
@@ -60,6 +68,7 @@ const query = groq`
 export default {
   components: {
     BlockContent,
+    Divider,
     LightBox,
     SanityImage,
     ShareMenu
@@ -68,9 +77,21 @@ export default {
     locale() { return this.$i18n.locale },
     image1Src() { return this.getImageUrl(this.image1) },
     image2Src() { return this.getImageUrl(this.image2) },
+    // lightboxImages() { return [
+    //   { src: this.image1Src },
+    //   { src: this.image2Src },
+    // ]}
     lightboxImages() { return [
-      { src: this.image1Src },
-      { src: this.image2Src },
+        {
+          image: this.image1,
+          src: builder.image(this.image1).url(),
+          thumb: builder.image(this.image1).width(200).url()
+        },
+        {
+          image: this.image2,
+          src: builder.image(this.image2).url(),
+          thumb: builder.image(this.image2).width(200).url()
+        },
     ]}
   },
   async asyncData({ params }) {
